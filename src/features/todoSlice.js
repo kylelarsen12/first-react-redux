44c484046152e21f4b539eeedee5ba18a2f5d4c7
unknown <kylelarsen12@gmail.com>
2022-08-16 17:10:05 -0400

    Create todoSlice actions

---
+++ src/features/todoSlice.js
@@ -0,0 +1,37 @@
+//import
+import { createSlice } from "@reduxjs/toolkit";
+
+//initial state
+const initialState = {
+  items: [],
+};
+
+//create slice
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    //use spread to get all aray items and add payload to it
+    addTodo: (state, action) => {
+      return { items: [...state.items, action.payload] };
+    },
+    //use spread to get all array items, determine which index of array to remove, use splice to remove that specific element
+    removeTodo: (state, action) => {
+      let todoList = [...state.items];
+      let itemIndex = action.payload;
+      if (itemIndex !== -1) {
+        todoList.splice((itemIndex, 1));
+        return { items: todoList };
+      }
+    },
+    //return an empty array
+    clearTodos: () => {
+      return { items: [] };
+    },
+  },
+});
+
+//exports
+export const { addTodo, removeTodo, clearTodos } = todoSlice.actions;
+
+export default todoSlice.reducer;
