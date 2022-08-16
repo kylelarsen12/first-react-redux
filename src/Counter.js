7dd3eacde5c03f11665bef6aeda20344c08ead55
unknown <kylelarsen12@gmail.com>
2022-08-15 21:28:38 -0400

    complete counterSlice and reducer actions for increment, decrement, and custom. Update display and add buttons

--- src/Counter.js
+++ src/Counter.js
@@ -1,3 +1,54 @@
-function Counter() {}
+//imports
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment, customAmount } from "./features/counterSlice";
+
+function Counter() {
+  //Call useSelector to obtain state value from store and reassign
+  const count = useSelector((state) => state.value);
+  //include useDispatch hook
+  const dispatch = useDispatch();
+
+  const [input, setInput] = useState(0);
+
+  const customAmountSubmit = (e) => {
+    e.preventDefault();
+    dispatch(customAmount(Number(input)));
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          dispatch(increment());
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch(decrement());
+        }}
+      >
+        Decrement
+      </button>
+
+      <form
+        onSubmit={(e) => {
+          customAmountSubmit(e);
+        }}
+      >
+        <input
+          type="number"
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+        ></input>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
 
 export default Counter;
